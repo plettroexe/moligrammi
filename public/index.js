16581c6ab9e7dc3d76d65d3d95531c9c5e08bb4a
Mirko <148048359+plettroexe@users.noreply.github.com>
2024-05-10 06:54:34 +0000

    organizzazione file e redirect

--- public/index.js
+++ public/index.js
@@ -1,46 +1,10 @@
-// username e password per il login
-const Nome = document.getElementById("Nome");
-const pass = document.getElementById("Password");
+const utenteLoggato = true; // questo codice va cambiato
 
-// bottone login
-const LoginButton = document.getElementById("Login");
-
-
-const Login = (ArrayUtenze) => {
-  return new Promise((resolve, reject) => {
-    fetch("/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(ArrayUtenze),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        resolve(json);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
-};
+if (utenteLoggato) {
+  document.location.href = "/home/home.html";
+} else {
+  document.location.href = "/login"
+}
+ 
 
 
-LoginButton.onclick = () => {
-  let saveArray = {
-    username: Nome.value,
-    password: pass.value,
-  };
-
-    Login(saveArray)
-    .then((response) => {
-      console.log("Response from server:", response);
-      if (response.result === "true") {
-          window.location.href = "/testClient/test.html";
-        }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      console.log("error");
-    });
-};
