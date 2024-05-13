ab7565a8de0276200033978343e175b3c19e1556
Mirko <148048359+plettroexe@users.noreply.github.com>
2024-05-13 07:34:03 +0000

    modifiche home

---
+++ public/home/home.js
@@ -0,0 +1,23 @@
+const home = document.getElementById("home");
+
+const templateCard = `
+<div class="col-md-4 mt-3">
+<div class="card">
+    <img src="data:image/jpeg;base64,{src}" class="card-img-top" alt="...">
+    <div class="card">
+                            <div class="card-body">
+                                <p class="card-like">Like 0</p>
+                                <p class="card-comments">Commenti 0</p>
+                            </div>
+                        </div>
+</div>
+</div>
+`;
+
+function renderPost() {
+
+    
+    
+}
+
+renderPost();
